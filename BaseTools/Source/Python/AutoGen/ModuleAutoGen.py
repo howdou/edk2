2706c5204fd5c84df57d20f36210e1be89af7a72
Shi, Steven <steven.shi@intel.com>
2019-07-21 16:43:05 +0800

    Add gBinCacheDest to tell not skip CreateMakeFile
    No matter produce or consume cache, both cases need CreateMakeFile

--- BaseTools/Source/Python/AutoGen/ModuleAutoGen.py
+++ BaseTools/Source/Python/AutoGen/ModuleAutoGen.py
@@ -1749,7 +1749,7 @@ class ModuleAutoGen(AutoGen):
 
 
         # Don't enable if cache feature enabled, CanSkip uses timestamps to determine build skipping
-        if not GlobalData.gBinCacheSource and self.CanSkip():
+        if not GlobalData.gBinCacheSource and not GlobalData.gBinCacheDest and self.CanSkip():
             return
 
         if len(self.CustomMakefile) == 0:
